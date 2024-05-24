4ec7fae78c29d4faafb28bf97a392a51b63daa48
hassan <hchoudhury20@hotmail.com>
2024-05-24 15:09:01 +0100

    lesson 1 complete

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
-print("hello world")
+for i in range(15):
+    if i % 3 == 0:
+        print("fizz")   
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
+###################################################
+
+def addTwonumber(num1,num2):
+    return num1+num2
+
+print(addTwonumber(2,322))
